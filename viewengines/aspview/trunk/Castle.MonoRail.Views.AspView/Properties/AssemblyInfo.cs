2409b94dae7fb1689ec40344b66cbf4b1fcad34e
ken.egozi <ken.egozi@73e77b4c-caa6-f847-a29a-24ab75ae54b6>
2007-05-20 05:52:20 +0000

    Updating AspView VersionInfo to 178

--- viewengines/aspview/trunk/Castle.MonoRail.Views.AspView/Properties/AssemblyInfo.cs
+++ viewengines/aspview/trunk/Castle.MonoRail.Views.AspView/Properties/AssemblyInfo.cs
@@ -8,5 +8,5 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyProduct("AspView MonoRail ViewEngine")]
 [assembly: AssemblyCopyright("Copyright 2006, 2007")]
 [assembly: ComVisible(false)]
-[assembly: AssemblyVersion("0.7.0.152")]
-[assembly: AssemblyFileVersion("0.7.0.152")]
+[assembly: AssemblyVersion("0.7.0.178")]
+[assembly: AssemblyFileVersion("0.7.0.178")]
