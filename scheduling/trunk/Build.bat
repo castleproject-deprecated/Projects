485d87da7c0be1d18b0e60cb48fd4eb652387012
jeff.brown <jeff.brown@73e77b4c-caa6-f847-a29a-24ab75ae54b6>
2007-06-14 08:25:35 +0000

    Improved the build scripts to make it easier to generate a release bundle.  We now copy the Db scripts to the build folder and automatically run all of the MbUnit tests.

--- scheduling/trunk/Build.bat
+++ scheduling/trunk/Build.bat
@@ -1,1 +1,3 @@
-@%SystemRoot%\Microsoft.NET\Framework\v2.0.50727\msbuild.exe "%~dp0\src\Castle.Components.Scheduler.sln" /p:Configuration=Release;OutputPath="%~dp0\build\bin" %*
+@echo off
+REM Builds everything and drops it in the Build folder.
+%SystemRoot%\Microsoft.NET\Framework\v2.0.50727\msbuild.exe "%~dp0\Build.msbuild" /v:minimal %*
