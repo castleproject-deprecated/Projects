c03ad08c91975263503d0e1712ea5e249c11f3d7
hammett <hammett@73e77b4c-caa6-f847-a29a-24ab75ae54b6>
2007-06-26 04:07:00 +0000

    Spaces to tabs

--- viewcomponents/trunk/Castle.MonoRail.ViewComponents/SmartGridComponent.cs
+++ viewcomponents/trunk/Castle.MonoRail.ViewComponents/SmartGridComponent.cs
@@ -16,212 +16,213 @@ namespace Castle.MonoRail.ViewComponents
 {
 	using System;
 	using System.Collections;
+	using System.Collections.Generic;
 	using System.Reflection;
 	using System.Text.RegularExpressions;
-	using System.Collections.Generic;
-	
+
 	public class SmartGridComponent : GridComponent
-    {
-        private static readonly Hashtable validTypesCache = Hashtable.Synchronized(new Hashtable());
-        private static readonly Hashtable propertiesCache = Hashtable.Synchronized(new Hashtable());
-        
+	{
+		private static readonly Hashtable validTypesCache = Hashtable.Synchronized(new Hashtable());
+		private static readonly Hashtable propertiesCache = Hashtable.Synchronized(new Hashtable());
+
 		private PropertyInfo[] properties;
 
-        public override bool SupportsSection(string name)
-        {
-            return true;
-        }
+		public override bool SupportsSection(string name)
+		{
+			return true;
+		}
 
-        protected override bool ShowRows(IEnumerable source)
-        {
-            if (properties == null) //there are no rows, if this is the case
-                return false;
-            
+		protected override bool ShowRows(IEnumerable source)
+		{
+			if (properties == null) //there are no rows, if this is the case
+				return false;
+
 			bool isAlternate = false;
-            
-			foreach (object item in source)
-            {
-                PropertyBag["item"] = item;
-                
+
+			foreach(object item in source)
+			{
+				PropertyBag["item"] = item;
+
 				if (isAlternate)
 				{
 					RenderText("<tr class='grid_alternateItem'>");
 				}
-                else
+				else
 				{
 					RenderText("<tr class='grid_item'>");
 				}
 
-                foreach (PropertyInfo property in properties)
-                {
-                    if (ValidPropertyToAutoGenerate(property) == false) continue;
-                    
+				foreach(PropertyInfo property in properties)
+				{
+					if (ValidPropertyToAutoGenerate(property) == false) continue;
+
 					if (Context.HasSection(property.Name))
-                    {
-                        PropertyBag["value"] = property.GetValue(item, null);
-                        Context.RenderSection(property.Name);
-                        continue;
-                    }
-                    
+					{
+						PropertyBag["value"] = property.GetValue(item, null);
+						Context.RenderSection(property.Name);
+						continue;
+					}
+
 					RenderStartCell();
-                    
+
 					object val = property.GetValue(item, null) ?? "null";
-                    
+
 					RenderText(val.ToString());
-                    RenderEndCell();
-                }
+					RenderEndCell();
+				}
 
-                if (Context.HasSection("more"))
-                {
-                    Context.RenderSection("more");
-                }
-                
+				if (Context.HasSection("more"))
+				{
+					Context.RenderSection("more");
+				}
+
 				isAlternate = !isAlternate;
-                RenderText("</tr>");
-            }
+				RenderText("</tr>");
+			}
 
-            return true;
-        }
+			return true;
+		}
 
-        private void RenderEndCell()
-        {
-            if (Context.HasSection("endCell"))
-            {
-                Context.RenderSection("endCell");
-                return;
-            }
-            RenderText("</td>");
-        }
+		private void RenderEndCell()
+		{
+			if (Context.HasSection("endCell"))
+			{
+				Context.RenderSection("endCell");
+				return;
+			}
+			RenderText("</td>");
+		}
 
-        private void RenderStartCell()
-        {
-            if (Context.HasSection("startCell"))
-            {
-                Context.RenderSection("startCell");
-                return;
-            }
-            RenderText("<td>");
-        }
+		private void RenderStartCell()
+		{
+			if (Context.HasSection("startCell"))
+			{
+				Context.RenderSection("startCell");
+				return;
+			}
+			RenderText("<td>");
+		}
 
-        protected override void ShowHeader(IEnumerable source)
-        {
-            IEnumerator enumerator = source.GetEnumerator();
-            
+		protected override void ShowHeader(IEnumerable source)
+		{
+			IEnumerator enumerator = source.GetEnumerator();
+
 			bool hasItem = enumerator.MoveNext();
-            
+
 			if (hasItem == false)
-            {
-                return;
-            }
-            
+			{
+				return;
+			}
+
 			object first = enumerator.Current;
-            InitializeProperties(first);
-            
-			foreach (PropertyInfo property in properties)
-            {
-                if (ValidPropertyToAutoGenerate(property) == false) continue;
-                string overrideSection = property.Name + "Header";
-                if (Context.HasSection(overrideSection))
-                {
-                    Context.RenderSection(overrideSection);
-                    continue;
-                }
-                RenderHeaderCellStart();
-                RenderText(SplitPascalCase(property.Name));
-                RenderHeaderCellEnd();
-            }
-            if (Context.HasSection("moreHeader"))
-            {
-                Context.RenderSection("moreHeader");
-            }
-        }
+			InitializeProperties(first);
 
-        private void RenderHeaderCellEnd()
-        {
-            if (Context.HasSection("endHeaderCell"))
-            {
-                Context.RenderSection("endHeaderCell");
-                return;
-            }
-            RenderText("</th>");
-        }
+			foreach(PropertyInfo property in properties)
+			{
+				if (ValidPropertyToAutoGenerate(property) == false) continue;
+				string overrideSection = property.Name + "Header";
+				if (Context.HasSection(overrideSection))
+				{
+					Context.RenderSection(overrideSection);
+					continue;
+				}
+				RenderHeaderCellStart();
+				RenderText(SplitPascalCase(property.Name));
+				RenderHeaderCellEnd();
+			}
+			if (Context.HasSection("moreHeader"))
+			{
+				Context.RenderSection("moreHeader");
+			}
+		}
 
-        private void RenderHeaderCellStart()
-        {
-            if (Context.HasSection("startHeaderCell"))
-            {
-                Context.RenderSection("startHeaderCell");
-                return;
-            }
-            RenderText("<th class='grid_header'>");
-        }
+		private void RenderHeaderCellEnd()
+		{
+			if (Context.HasSection("endHeaderCell"))
+			{
+				Context.RenderSection("endHeaderCell");
+				return;
+			}
+			RenderText("</th>");
+		}
 
-        /// <summary>
-        /// Split a PascalCase string into Pascal Case words.
-        /// Note that if the string contains spaces, we assume it is already formatted
-        /// http://weblogs.asp.net/jgalloway/archive/2005/09/27/426087.aspx
-        /// </summary>
-        private static string SplitPascalCase(string input)
-        {
-            if (input.Contains(" ")) return input;
-            return Regex.Replace(input, "([A-Z])", " $1", RegexOptions.Compiled);
-        }
+		private void RenderHeaderCellStart()
+		{
+			if (Context.HasSection("startHeaderCell"))
+			{
+				Context.RenderSection("startHeaderCell");
+				return;
+			}
+			RenderText("<th class='grid_header'>");
+		}
 
-        private void InitializeProperties(object first)
-        {
-            Type type = first.GetType();
-            if (ComponentParams.Contains("columns") == false)
-            {
-                if (propertiesCache.Contains(type))
-                    properties = (PropertyInfo[])propertiesCache[type];
-                else
-                    propertiesCache[type] = properties = type.GetProperties();
-                return;
-            }
-            List<PropertyInfo> props = new List<PropertyInfo>();
-            IEnumerable columns = (IEnumerable)ComponentParams["columns"];
-            foreach (string columnName in columns)
-            {
-                string key = type.FullName + "." + columnName;
-                PropertyInfo propertyInfo;
-                if (propertiesCache.Contains(key))
-                    propertyInfo = (PropertyInfo)propertiesCache[key];
-                else
-                    propertiesCache[key] = propertyInfo = type.GetProperty(columnName, BindingFlags.Instance | BindingFlags.Public | BindingFlags.IgnoreCase);
-                if (propertyInfo != null)
-                    props.Add(propertyInfo);
-            }
-            properties = props.ToArray();
-        }
+		/// <summary>
+		/// Split a PascalCase string into Pascal Case words.
+		/// Note that if the string contains spaces, we assume it is already formatted
+		/// http://weblogs.asp.net/jgalloway/archive/2005/09/27/426087.aspx
+		/// </summary>
+		private static string SplitPascalCase(string input)
+		{
+			if (input.Contains(" ")) return input;
+			return Regex.Replace(input, "([A-Z])", " $1", RegexOptions.Compiled);
+		}
 
-        private bool ValidPropertyToAutoGenerate(PropertyInfo property)
-        {
-            if (false.Equals(ComponentParams["Display" + property.Name]))
-                return false;
-            if (Context.HasSection(property.Name))
-                return true;
-            return IsValidType(property.PropertyType);
-        }
+		private void InitializeProperties(object first)
+		{
+			Type type = first.GetType();
+			if (ComponentParams.Contains("columns") == false)
+			{
+				if (propertiesCache.Contains(type))
+					properties = (PropertyInfo[]) propertiesCache[type];
+				else
+					propertiesCache[type] = properties = type.GetProperties();
+				return;
+			}
+			List<PropertyInfo> props = new List<PropertyInfo>();
+			IEnumerable columns = (IEnumerable) ComponentParams["columns"];
+			foreach(string columnName in columns)
+			{
+				string key = type.FullName + "." + columnName;
+				PropertyInfo propertyInfo;
+				if (propertiesCache.Contains(key))
+					propertyInfo = (PropertyInfo) propertiesCache[key];
+				else
+					propertiesCache[key] =
+						propertyInfo = type.GetProperty(columnName, BindingFlags.Instance | BindingFlags.Public | BindingFlags.IgnoreCase);
+				if (propertyInfo != null)
+					props.Add(propertyInfo);
+			}
+			properties = props.ToArray();
+		}
 
-        private static bool IsValidType(Type typeToCheck)
-        {
-            if (validTypesCache.ContainsKey(typeToCheck))
-                return (bool)validTypesCache[typeToCheck];
-            bool result;
-            if (typeof(ICollection).IsAssignableFrom(typeToCheck))
-            {
-                result = false;
-            }
-            else if (typeToCheck.IsGenericType)
-            {
-                result = typeof(ICollection<>).IsAssignableFrom(typeToCheck.GetGenericTypeDefinition());
-            }
-            else
-            {
-                result = true;
-            }
-            validTypesCache[typeToCheck] = result;
-            return result;
-        }
-    }
+		private bool ValidPropertyToAutoGenerate(PropertyInfo property)
+		{
+			if (false.Equals(ComponentParams["Display" + property.Name]))
+				return false;
+			if (Context.HasSection(property.Name))
+				return true;
+			return IsValidType(property.PropertyType);
+		}
+
+		private static bool IsValidType(Type typeToCheck)
+		{
+			if (validTypesCache.ContainsKey(typeToCheck))
+				return (bool) validTypesCache[typeToCheck];
+			bool result;
+			if (typeof(ICollection).IsAssignableFrom(typeToCheck))
+			{
+				result = false;
+			}
+			else if (typeToCheck.IsGenericType)
+			{
+				result = typeof(ICollection<>).IsAssignableFrom(typeToCheck.GetGenericTypeDefinition());
+			}
+			else
+			{
+				result = true;
+			}
+			validTypesCache[typeToCheck] = result;
+			return result;
+		}
+	}
 }
