ff521ee42178036692f034e9e8e9a411da176bb7
ken.egozi <ken.egozi@73e77b4c-caa6-f847-a29a-24ab75ae54b6>
2007-07-15 22:18:28 +0000

    support for multi view assemblies

---
+++ viewengines/aspview/trunk/AspViewTestAdditionalViewsSite/Controllers/AdditionalController.cs
@@ -0,0 +1,10 @@
+using System;
+using Castle.MonoRail.Framework;
+
+namespace AspViewTestAdditionalViewsSite.Controllers
+{
+	public class AdditionalController : Controller
+	{
+		public void Index() { }
+	}
+}
