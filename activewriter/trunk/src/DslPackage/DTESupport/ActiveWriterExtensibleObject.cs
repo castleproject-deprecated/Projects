e1a411f0580c9ee8017cd6a5d9aaa34b15adaac9
gokhan <gokhan@73e77b4c-caa6-f847-a29a-24ab75ae54b6>
2007-09-11 08:24:19 +0000

    Added DTE support to access AW objects and diagrams from addins and macros. See: http://forums.microsoft.com/MSDN/ShowPost.aspx?PostID=476202&SiteID=1
    Syntax:
    ActiveWriterExtensibleObject o1 = (ActiveWriterExtensibleObject)dte.ActiveDocument.Object("Model");
    ActiveWriterDiagramExtensibleObject o2 = (ActiveWriterDiagramExtensibleObject)dte.ActiveWindow.Object;
    
    To make it work with macros or for other out of process calls, you need to export AW assemblies with tlbexp and register so that CCW can do the casting. You can use implemented interfaces IActiveWriterExtensibleObject and IActiveWriterDiagramExtensibleObject when needed.
    
    This is mainly experimental.

---
+++ activewriter/trunk/src/DslPackage/DTESupport/ActiveWriterExtensibleObject.cs
@@ -0,0 +1,34 @@
+// Copyright 2006 Gokhan Altinoren - http://altinoren.com/
+// 
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// 
+//     http://www.apache.org/licenses/LICENSE-2.0
+// 
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+namespace Altinoren.ActiveWriter
+{
+    using System.Runtime.InteropServices;
+
+    [ComVisible(true)]
+    public class ActiveWriterExtensibleObject : IActiveWriterExtensibleObject
+    {
+        private Model _model;
+
+        public ActiveWriterExtensibleObject(Model model)
+        {
+            this._model = model;
+        }
+
+        public Model Model
+        {
+            get { return _model; }
+        }
+    }
+}
