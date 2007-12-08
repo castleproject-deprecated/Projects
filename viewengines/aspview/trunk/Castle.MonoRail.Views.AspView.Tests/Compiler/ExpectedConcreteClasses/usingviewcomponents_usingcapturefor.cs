31c7abba50e3ca37e6e15ec46099207eaa5b3bb9
ken.egozi <ken.egozi@73e77b4c-caa6-f847-a29a-24ab75ae54b6>
2007-12-08 13:51:38 +0000

    CaptureFor instead of CaptureFor2

--- viewengines/aspview/trunk/Castle.MonoRail.Views.AspView.Tests/Compiler/ExpectedConcreteClasses/usingviewcomponents_usingcapturefor.cs
+++ viewengines/aspview/trunk/Castle.MonoRail.Views.AspView.Tests/Compiler/ExpectedConcreteClasses/usingviewcomponents_usingcapturefor.cs
@@ -16,7 +16,7 @@ namespace CompiledViews
 		{
 Output(@"a. Some text, located before the capturedContent component
 ");
-InvokeViewComponent("CaptureFor2", CaptureFor20_body, null, "id", "capturedContent");
+InvokeViewComponent("CaptureFor", CaptureFor0_body, null, "id", "capturedContent");
 Output(@"
 b. Some text, located after the capturedContent component
 This text should be rendered right after text b.
@@ -24,7 +24,7 @@ This text should be rendered right after text b.
 
 		}
 
-		internal void CaptureFor20_body ()
+		internal void CaptureFor0_body ()
 		{
 			Output(@"
 			This content should be rendered in the captured-for place holder
--- viewengines/aspview/trunk/Castle.MonoRail.Views.AspView.Tests/Compiler/ExpectedConcreteClasses/usingviewcomponents_usingmultipleviewcomponents.cs
+++ viewengines/aspview/trunk/Castle.MonoRail.Views.AspView.Tests/Compiler/ExpectedConcreteClasses/usingviewcomponents_usingmultipleviewcomponents.cs
@@ -17,10 +17,10 @@ namespace CompiledViews
 		{
 Output(@"Some view text
 ");
-InvokeViewComponent("CaptureFor2", CaptureFor20_body, null, "id", "capturedContent1");
+InvokeViewComponent("CaptureFor", CaptureFor0_body, null, "id", "capturedContent1");
 Output(@"
 ");
-InvokeViewComponent("CaptureFor2", CaptureFor21_body, null, "id", "capturedContent2");
+InvokeViewComponent("CaptureFor", CaptureFor1_body, null, "id", "capturedContent2");
 Output(@"
 Some view text
 The next text should be bolded:
@@ -31,7 +31,7 @@ Some view text - not bolded");
 
 		}
 
-		internal void CaptureFor20_body ()
+		internal void CaptureFor0_body ()
 		{
 			Output(@"
 			This content should be rendered in the captured-for place holder no. 1
@@ -39,7 +39,7 @@ Some view text - not bolded");
 
 		}
 
-		internal void CaptureFor21_body ()
+		internal void CaptureFor1_body ()
 		{
 			Output(@"
 			This content should be rendered in the captured-for place holder no. 2
