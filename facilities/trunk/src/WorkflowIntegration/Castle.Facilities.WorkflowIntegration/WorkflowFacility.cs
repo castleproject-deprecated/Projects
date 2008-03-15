99b24a3b8d2e29944c95012358b156ffefe2ad6f
loudej <loudej@73e77b4c-caa6-f847-a29a-24ab75ae54b6>
2008-03-15 22:38:25 +0000

    Conforming to coding standards

--- facilities/trunk/src/WorkflowIntegration/Castle.Facilities.WorkflowIntegration/WorkflowFacility.cs
+++ facilities/trunk/src/WorkflowIntegration/Castle.Facilities.WorkflowIntegration/WorkflowFacility.cs
@@ -1,14 +1,47 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-using System.Workflow.Runtime;
-using Castle.MicroKernel.Facilities;
-using Castle.MicroKernel;
-using System.Workflow.Runtime.Hosting;
-using System.Workflow.Activities;
+﻿// Copyright 2004-2007 Castle Project - http://www.castleproject.org/
+// 
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// 
+//     http://www.apache.org/licenses/LICENSE-2.0
+// 
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 
 namespace Castle.Facilities.WorkflowIntegration
 {
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+
+    using System.Workflow.Runtime;
+    using System.Workflow.Runtime.Hosting;
+    using System.Workflow.Activities;
+    
+    using Castle.MicroKernel.Facilities;
+    using Castle.MicroKernel;
+    
+    /// <summary>
+    /// Facility which creates an instance of the WorkflowRuntime class
+    /// and adds it to the Windsor container as an instance with a singleton
+    /// lifestyle. Application classes created by the Windor container gain access to the
+    /// workflow runtime through a constructor argument or public property.
+    /// 
+    /// Components that implement [ExternalDataExchange] attributed interfaces 
+    /// are instantiated when they are added to the container and made available 
+    /// to the WorkflowRuntime. This enables the CallExternalMethod and HandleExternalEvent 
+    /// activities to utilize the interface's members.
+    /// 
+    /// Components that are descended from WorkflowRuntimeServices are instantiated
+    /// and added as services to the WorkflowRuntime. This allows for workflow services
+    /// such as persistence and tracking to be declared and configured without code.
+    /// 
+    /// </summary>
     public class WorkflowFacility : AbstractFacility
     {
         WorkflowRuntime _runtime;        
