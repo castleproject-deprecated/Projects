873a2f4a4d48bcaecc2b2a09d81cddf3004b333d
lee.m.henson <lee.m.henson@73e77b4c-caa6-f847-a29a-24ab75ae54b6>
2008-04-13 14:57:56 +0000

    Used DictionaryAdapter attribute to resolve helpers from the Properties collection using the form "<Name>Helper" rather than just "<Name>". Ie. UrlHelper not Url, as it was easy to break this mechanism by setting a property in your controller, e.g. Properties["Url"] = "a url I need to display in my view", which would break dictionaryAdapterFactory.Get<IHelpersAccesor>().Url.

--- viewengines/aspview/trunk/Castle.MonoRail.Views.AspView/IHelpersAccesor.cs
+++ viewengines/aspview/trunk/Castle.MonoRail.Views.AspView/IHelpersAccesor.cs
@@ -1,3 +1,4 @@
+using Castle.Components.DictionaryAdapter;
 using Castle.MonoRail.Framework.Helpers;
 
 namespace Castle.MonoRail.Views.AspView
@@ -7,66 +8,79 @@ namespace Castle.MonoRail.Views.AspView
 		/// <summary>
 		/// Gets the AjaxHelper instance
 		/// </summary>
+		[DictionaryKeySubstitution("Ajax", "AjaxHelper")]
 		AjaxHelper Ajax { get; }
 
 		/// <summary>
 		/// Gets the BehaviourHelper instance
 		/// </summary>
+		[DictionaryKeySubstitution("Behaviour", "BehaviourHelper")]
 		BehaviourHelper Behaviour { get; }
 
 		/// <summary>
 		/// Gets the DateFormatHelper instance
 		/// </summary>
+		[DictionaryKeySubstitution("DateFormat", "DateFormatHelper")]
 		DateFormatHelper DateFormat { get; }
 
 		/// <summary>
 		/// Gets the DictHelper instance
 		/// </summary>
+		[DictionaryKeySubstitution("Dict", "DictHelper")]
 		DictHelper Dict { get; }
 
 		/// <summary>
 		/// Gets the EffectsFatHelper instance
 		/// </summary>
+		[DictionaryKeySubstitution("EffectsFat", "EffectsFatHelper")]
 		EffectsFatHelper EffectsFat { get; }
 
 		/// <summary>
 		/// Gets the FormHelper instance
 		/// </summary>
+		[DictionaryKeySubstitution("Form", "FormHelper")]
 		FormHelper Form { get; }
 
 		/// <summary>
 		/// Gets the HtmlHelper instance
 		/// </summary>
+		[DictionaryKeySubstitution("Html", "HtmlHelper")]
 		HtmlHelper Html { get; }
 
 		/// <summary>
 		/// Gets the PaginationHelper instance
 		/// </summary>
+		[DictionaryKeySubstitution("Pagination", "PaginationHelper")]
 		PaginationHelper Pagination { get; }
 
 		/// <summary>
 		/// Gets the ScriptaculousHelper instance
 		/// </summary>
+		[DictionaryKeySubstitution("Scriptaculous", "ScriptaculousHelper")]
 		ScriptaculousHelper Scriptaculous { get; }
 
 		/// <summary>
 		/// Gets the UrlHelper instance
 		/// </summary>
+		[DictionaryKeySubstitution("Url", "UrlHelper")]
 		UrlHelper Url { get; }
 
 		/// <summary>
 		/// Gets the ValidationHelper instance
 		/// </summary>
+		[DictionaryKeySubstitution("Validation", "ValidationHelper")]
 		ValidationHelper Validation { get; }
 
 		/// <summary>
 		/// Gets the WizardHelper instance
 		/// </summary>
+		[DictionaryKeySubstitution("Wizard", "WizardHelper")]
 		WizardHelper Wizard { get; }
 
 		/// <summary>
 		/// Gets the ZebdaHelper instance
 		/// </summary>
+		[DictionaryKeySubstitution("Zebda", "ZebdaHelper")]
 		ZebdaHelper Zebda { get; }
 	}
 }
