91665e13d2a41f7bfc344dc2a2f8a1b9c6ea9b18
roelofb <roelofb@73e77b4c-caa6-f847-a29a-24ab75ae54b6>
2008-06-05 22:43:00 +0000

    Fixed CONTRIB-113. Patch submitted by Gauthier Segay.

--- Castle.MonoRail.Rest/trunk/Source/Castle.MonoRail.Rest/Responder.cs
+++ Castle.MonoRail.Rest/trunk/Source/Castle.MonoRail.Rest/Responder.cs
@@ -23,6 +23,15 @@ namespace Castle.MonoRail.Rest
 	using System.Xml.Serialization;
 	using System.Collections.Specialized;
 
+	public static class RepsonderExtensions 
+	{
+		public static void Serialize(this Responder responder, Object obj)
+		{
+			XmlSerializer serial = new XmlSerializer(obj.GetType());
+			responder.SerializeWith(serial.Serialize, obj);
+		}
+	}
+
 	public class Responder
 	{
 		private IControllerBridge _controllerBridge;
@@ -35,23 +44,40 @@ namespace Castle.MonoRail.Rest
 			_controllerBridge = controllerBridge;
 			_controllerAction = controllerAction;
 		}
-	
 
+		/// <summary>
+		/// Renders the according view such as actionname_format
+		/// <example>
+		/// <list type="table">
+		/// <listheader><term>context item</term><description>sample</description></listheader>
+		/// <item><term>action</term><description>ControllerType.ActionName</description></item>
+		/// <item><term>format</term><description>xml</description></item>
+		/// </list>
+		///		will pick /Views/ControllerType/ActionName_xml template
+		/// </example>
+		/// </summary>
 		public void DefaultResponse()
 		{
 			_controllerBridge.SendRenderView(_controllerAction + "_" + Format);
 		}
 
-		public void Serialize(Object obj)
+		/// <summary>
+		/// Serialize given object with provided serialization action adapter
+		/// </summary>
+		/// <typeparam name="T">object type to be serialized</typeparam>
+		/// <param name="serializationActionAdapter">delegate accepting TextWriter and object that will perform serialization</param>
+		/// <param name="objectToSerialize">object to be serialized</param>
+		public void SerializeWith<T>(Action<System.IO.TextWriter, T> serializationActionAdapter, T objectToSerialize) 
 		{
-
 			_controllerBridge.SendCancelLayoutAndView();
-
-			XmlSerializer serial = new XmlSerializer(obj.GetType());
-			_controllerBridge.UseResponseWriter(writer => serial.Serialize(writer, obj));
-
+			_controllerBridge.UseResponseWriter(writer => serializationActionAdapter(writer, objectToSerialize));
 		}
-
+    
+		/// <summary>
+		/// Sends an empty HTTP response appending given headers to the HTTP Response
+		/// </summary>
+		/// <param name="statusCode">HTTP response status code</param>
+		/// <param name="addHeaders">Additional HTTP headers population delegate</param>
 		public void Empty(int statusCode, Action<IDictionary<string, string>> addHeaders)
 		{
 			Dictionary<string, string> headers = new Dictionary<string, string>();
@@ -70,11 +96,20 @@ namespace Castle.MonoRail.Rest
 			_controllerBridge.SendCancelLayoutAndView();
 		}
 
+		/// <summary>
+		/// Sends an empty HTTP response
+		/// </summary>
+		/// <param name="statusCode">HTTP response status code</param>
+		/// <param name="addHeaders">Additional HTTP headers population delegate</param>
 		public void Empty(int statusCode)
 		{
 			Empty(statusCode, null);
 		}
 
+		/// <summary>
+		/// Sends given text as HTTP response content
+		/// </summary>
+		/// <param name="text">content</param>
 		public void Text(string text)
 		{
 			_controllerBridge.SendRenderText(text);
