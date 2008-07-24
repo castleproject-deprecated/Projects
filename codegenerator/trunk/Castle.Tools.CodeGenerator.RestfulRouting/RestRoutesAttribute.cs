1cb510268bba3ef5a99abf2f56c0180ff664d709
lee.m.henson <lee.m.henson@73e77b4c-caa6-f847-a29a-24ab75ae54b6>
2008-07-24 17:52:10 +0000

    Starts to add support for generating RouteDefinitions for RestRoutes using a RestRoutesAttribute.

---
+++ codegenerator/trunk/Castle.Tools.CodeGenerator.RestfulRouting/RestRoutesAttribute.cs
@@ -0,0 +1,33 @@
+using System;
+using Castle.MonoRail.Rest;
+
+namespace Castle.Tools.CodeGenerator.RestfulRouting
+{
+	[AttributeUsage(AttributeTargets.Class, AllowMultiple = false, Inherited = false)]
+	public class RestRoutesAttribute : Attribute
+	{
+		public RestRoutesAttribute(string name, string collection, string identifier)
+		{
+			Name = name;
+			Collection = collection;
+			Identifier = identifier;
+		}
+
+		public RestRoutesAttribute(string name, string collection, string identifier, Type resolverType)
+		{
+			Name = name;
+			Collection = collection;
+			Identifier = identifier;
+			
+			if (!typeof(IRestVerbResolver).IsAssignableFrom(resolverType) || !resolverType.IsClass)
+				throw new ArgumentException("The resolverType must be a class that implements IRestVerbResolver");
+
+			RestResolver = resolverType;
+		}
+
+		public string Name { get; private set; }
+		public string Collection { get; private set; }
+		public string Identifier { get; private set; }
+		public Type RestResolver { get; private set; }
+	}
+}
