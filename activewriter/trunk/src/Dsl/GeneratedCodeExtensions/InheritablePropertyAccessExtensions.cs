f4243823e9d514a418eeb472ca0c172b72fd9d78
patearl <patearl@73e77b4c-caa6-f847-a29a-24ab75ae54b6>
2009-08-10 06:37:03 +0000

    Property access is now inheritable in model (missed a file last commit).

---
+++ activewriter/trunk/src/Dsl/GeneratedCodeExtensions/InheritablePropertyAccessExtensions.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Altinoren.ActiveWriter
+{
+    public static class InheritablePropertyAccessExtensions
+    {
+        public static PropertyAccess GetMatchingPropertyAccess(this InheritablePropertyAccess type)
+        {
+            try
+            {
+                return (PropertyAccess)Enum.Parse(typeof(PropertyAccess), type.ToString());
+            }
+            catch (ArgumentException)
+            {
+                throw new ArgumentOutOfRangeException("type", "InheritedRelationType can only be used as a hint to determine the derived relation's RelationType relative to the Model.");
+            }
+        }
+    }
+}
