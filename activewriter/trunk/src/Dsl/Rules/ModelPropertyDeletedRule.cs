5d1482d693b7fed7cb3ba484f904b2907c0f816f
hconceicao <henry.conceicao@gmail.com>
2010-09-17 20:16:53 -0400

    updated license header

--- activewriter/trunk/src/Dsl/Rules/ModelPropertyDeletedRule.cs
+++ activewriter/trunk/src/Dsl/Rules/ModelPropertyDeletedRule.cs
@@ -1,4 +1,4 @@
-// Copyright 2006 Gokhan Castle - http://altinoren.com/
+// Copyright 2004-2010 Castle Project - http://www.castleproject.org/
 // 
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
